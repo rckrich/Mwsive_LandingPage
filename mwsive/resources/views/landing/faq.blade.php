24b4f26b04ab2558b426d4c0ec2230c2d2f103dd
Edith Quijano <equijanoricalde@gmail.com>
2023-11-27 19:55:08 -0600

    FAQ fixes

--- mwsive/resources/views/landing/faq.blade.php
+++ mwsive/resources/views/landing/faq.blade.php
@@ -16,11 +16,11 @@
 
                 <div class="accordion" id="accordionFAQ">
                     <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <div class="accordion-header">
+                            <div class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 {{__('faq.q1')}}
-                            </button>
-                        </h2>
+                            </div>
+                        </div>
                         <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionFAQ">
                             <div class="accordion-body">
                                 <p>{{__('faq.r1')}}</p>
@@ -28,11 +28,11 @@
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <div class="accordion-header">
+                            <div class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 {{__('faq.q2')}}
-                            </button>
-                        </h2>
+                            </div>
+                        </div>
                         <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFAQ">
                             <div class="accordion-body">
                                 <p>{{__('faq.r2')}}</p>
@@ -40,11 +40,11 @@
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <div class="accordion-header">
+                            <div class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                 {{__('faq.q3')}}
-                            </button>
-                        </h2>
+                            </div>
+                        </div>
                         <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFAQ">
                             <div class="accordion-body">
                                 <p>{{__('faq.r3_1')}}</p>
@@ -53,11 +53,11 @@
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <div class="accordion-header">
+                            <div class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                                 {{__('faq.q4')}}
-                            </button>
-                        </h2>
+                            </div>
+                        </div>
                         <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFAQ">
                             <div class="accordion-body">
                                 <p>{{__('faq.r4')}}</p>
@@ -65,12 +65,12 @@
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <div class="accordion-header">
+                            <div class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                                 {{__('faq.q5')}}
-                            </button>
-                        </h2>
-                        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFAQ">
+                            </div>
+                        </div>
+                        <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFAQ">
                             <div class="accordion-body">
                                 <p>{{__('faq.r5')}}</p>
                             </div>
