b564552e5fcd4baf41f01e44458d36f293f0b7f3
Edith Quijano <equijanoricalde@gmail.com>
2023-12-18 16:41:22 -0600

    Update TyC file

--- mwsive/resources/views/landing/external_links.blade.php
+++ mwsive/resources/views/landing/external_links.blade.php
@@ -11,15 +11,15 @@
                 </div>
             </div>
             <div class="row">
-                
+
                 <div class="col-12">
-                    <p><a href="https://www.tiktok.com/@mwsive">{{__('tc.sample1')}}</a></p>
+                    <p><a href="{{asset('assets/docs/Mwsive TyC.pdf')}}">{{__('tc.sample1')}}</a></p>
+                    <!--
                     <p><a href="https://www.instagram.com/mwsive/?hl=en">{{__('tc.sample2')}}</a></p>
                     <p><a href="https://twitter.com/mwsive">{{__('tc.sample3')}}</a></p>
+                    -->
                 </div>
-               
             </div>
         </div>
     </section>
-    
 @endsection
